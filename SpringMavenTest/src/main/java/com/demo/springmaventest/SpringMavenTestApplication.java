977e088b3f0b53c0505526a1388e3046ba984fef
jongwon-jeon <jw6963@naver.com>
2025-03-04 15:19:08 +0900

    init commit

---
+++ SpringMavenTest/src/main/java/com/demo/springmaventest/SpringMavenTestApplication.java
@@ -0,0 +1,14 @@
+package com.demo.springmaventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringMavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenTestApplication.class, args);
+	}
+
+}
---
+++ SpringMavenTest/src/test/java/com/demo/springmaventest/SpringMavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springmaventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
